d011cf82d1de051997c4d0dcc9bff4a9ead10f4f
lufansheng <lufansheng@nep-logistics.com>
2026-06-16 12:27:37 +0800

    fix: 修复 Sanity Studio 结构菜单 ID

--- studio/structure.ts
+++ studio/structure.ts
@@ -1,12 +1,14 @@
 import type { StructureResolver } from 'sanity/structure';
 
 export const structure: StructureResolver = (S) => S.list()
+  .id('content-management')
   .title('内容管理')
   .items([
     S.documentTypeListItem('photo').title('相册'),
     S.documentTypeListItem('note').title('文章'),
     S.documentTypeListItem('project').title('项目'),
     S.listItem()
+      .id('profile-singleton')
       .title('个人资料')
       .child(
         S.document()
